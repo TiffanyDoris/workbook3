26258a3231a87ddb3ababd97c51d85f8e871af7b
tiffanydoris <tiffanydoris12@gmail.com>
2024-04-29 15:59:08 -0400

    wrkb3

--- demos/add.js
+++ demos/add.js
@@ -1,10 +1,9 @@
 function add(number1, number2) {
-var number1 = number1 + number2;
-console.log(result);
+  var number1 = number1 + number2;
+  console.log(result);
 }
 
-
-var x= 5;
+var x = 5;
 var y = 6;
 
 add(x, y);

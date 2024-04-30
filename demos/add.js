a3e11d2cb8dd396a14e934b0a9d79e4ec0ff9ad7
tiffanydoris <tiffanydoris12@gmail.com>
2024-04-30 09:54:54 -0400

    demos.4/30

--- demos/add.js
+++ demos/add.js
@@ -3,7 +3,18 @@ function add(number1, number2) {
   console.log(result);
 }
 
-var x = 5;
-var y = 6;
+var x = 55;
+var y = 62;
 
 add(x, y);
+
+add(1,1)
+add(2,2)
+add(3,3)
+
+
+function bake(ingredient1, ingredient2, ingredient3) {
+    if(ingredient1 == "apples"){
+      console.log("apple pie");
+    }
+}
---
+++ demos/more_functions.js
@@ -0,0 +1,7 @@
+function displayMailingLabel(name, address, city, state, zip) {}
+//console.log(name, address, city, state, zip);
+console.log(name);
+console.log(address);
+console.log(city + state + address);
+
+displayMailingLabel("Tiffany", "Pitkin Ave", "Brooklyn", "New York", "11212");

d500ae39f88712734194cab547cfc06eb7062951
tiffanydoris <tiffanydoris12@gmail.com>
2024-04-30 13:05:48 -0400

    excersise 1

---
+++ demos/add-return.js
@@ -0,0 +1,18 @@
+function add(number1, number2) {
+  var result = number1 + number2;
+  return result;
+}
+
+//a
+var resultCaught = add(2, 2);
+console.log(resultCaught);
+//OR
+
+//b
+console.log(add(2, 2));
+
+function loadPosts() {
+  //stuff
+  //call api and get data
+  // return posts
+}
--- demos/more_functions.js
+++ demos/more_functions.js
@@ -1,7 +1,20 @@
-function displayMailingLabel(name, address, city, state, zip) {}
-//console.log(name, address, city, state, zip);
-console.log(name);
-console.log(address);
-console.log(city + state + address);
+"use strict";
 
-displayMailingLabel("Tiffany", "Pitkin Ave", "Brooklyn", "New York", "11212");
+function displayReceipt(totalDue, amountPaid) {
+  var change = amountPaid - totalDue;
+  console.log("Total Due: $" + totalDue);
+  console.log("Amount Paid: $" + amountPaid);
+
+  if (change > 0) {
+    console.log("Change $" + change);
+  } else if (change == 0) {
+    console.log("No change needed");
+  } else if (change < 0) {
+    console.log("You still owe another: $" + Math.abs(change));
+  }
+  console.log();
+}
+
+displayReceipt(11.75, 20);
+displayReceipt(11.75, 11.75);
+displayReceipt(11.75, 10);
